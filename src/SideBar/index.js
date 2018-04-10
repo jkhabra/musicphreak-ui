a31aa49f6edc7f7396aad9a1fca637b01104f423
Jatinder Singh <jkhabra09@gmail.com>
2018-04-10 10:09:45 +0530

    Add component for sidebar

---
+++ src/SideBar/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+class SideBar extends Component {
+  render() {
+    return (
+      <div>
+        <div className="browse-section">
+          <ul className="main-links">
+            <li className="main-title">Browse</li>
+            <li className="main-item">
+              <a className="main-link home" href="/">
+                Home
+              </a>
+            </li>
+            <li className="main-item">
+              <a className="main-link" href="#palylists">
+                Playlists
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="my-music">
+          <ul className="main-links">
+            <li className="main-title my">My music</li>
+            <li className="my-item">
+              <a className="main-link my" href="/">
+                Albums
+              </a>
+            </li>
+            <li className="my-item">
+              <a className="main-link my" href="#palylists">
+                Artists
+              </a>
+            </li>
+            <li className="my-item">
+              <a className="main-link my" href="#palylists">
+                Songs
+              </a>
+            </li>
+            <li className="my-item">
+              <a className="main-link my" href="#palylists">
+                Genres
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SideBar;
